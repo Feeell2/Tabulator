360fbb700934c9e43bd32cc7bf91d64dcd6b839c
Feeell2 <tomek.filinski@gmail.com>
2020-02-18 23:37:49 +0100

    Updates

---
+++ precache-manifest.c68103b1cc0c2d7e213c99adf1d9c8f5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "261b55a77fcdf705ecf40f70fe01a0de",
+    "url": "/Tabulator/index.html"
+  },
+  {
+    "revision": "524caa736d485552529d",
+    "url": "/Tabulator/static/css/main.442ecd4b.chunk.css"
+  },
+  {
+    "revision": "4e31592e800e7edbd6e7",
+    "url": "/Tabulator/static/js/2.034ea1ab.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Tabulator/static/js/2.034ea1ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "524caa736d485552529d",
+    "url": "/Tabulator/static/js/main.8822c6c6.chunk.js"
+  },
+  {
+    "revision": "5ed44524519b9b0a8c9f",
+    "url": "/Tabulator/static/js/runtime-main.295e72d7.js"
+  },
+  {
+    "revision": "c57d508cc36e1fa833309e3e33ed525e",
+    "url": "/Tabulator/static/media/music-3735836_1280.c57d508c.jpg"
+  },
+  {
+    "revision": "9900329852204a492ee61b6fa170585b",
+    "url": "/Tabulator/static/media/music-3735836_640.99003298.jpg"
+  }
+]);
